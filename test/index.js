35ca1ef52b09e4fb9d75f75b59092298563a556e
Eric Jennings <ericj@loopshot.com>
2011-09-02 16:30:30 -0700

    Added ability to delete all documents of "_type" field

--- test/index.js
+++ test/index.js
@@ -100,11 +100,81 @@ exports['delete document'] = function (test) {
     });
 };
 
-exports['cleanup index'] = function (test) {
-    if (path.existsSync(indexPath)) {
-        wrench.rmdirSyncRecursive(indexPath);
-    }
-    test.done();
+exports['add doc1 for type test'] = function (test) {
+    var doc = new cl.Document();
+    var docId = '10';
+
+    doc.addField('name', 'Eric Jennings', cl.STORE_YES|cl.INDEX_TOKENIZED);
+    doc.addField('_type', 'contact', cl.STORE_YES|cl.INDEX_UNTOKENIZED);
+    doc.addField('timestamp', '1293765885000', cl.STORE_YES|cl.INDEX_UNTOKENIZED);
+
+    clucene.addDocument(docId, doc, indexPath, function(err, indexTime, docsReplaced) {
+        test.equal(err, null);
+        test.ok(is('Number', indexTime));
+        test.equal(docsReplaced, 0);
+        test.done();
+    });
+};
+
+exports['add doc2 for type test'] = function (test) {
+    var doc = new cl.Document();
+    var docId = '11';
+
+    doc.addField('name', 'asdfasdf Jennings', cl.STORE_YES|cl.INDEX_TOKENIZED);
+    doc.addField('_type', 'contact', cl.STORE_YES|cl.INDEX_UNTOKENIZED);
+    doc.addField('timestamp', '1293765885000', cl.STORE_YES|cl.INDEX_UNTOKENIZED);
+
+    clucene.addDocument(docId, doc, indexPath, function(err, indexTime, docsReplaced) {
+        test.equal(err, null);
+        test.ok(is('Number', indexTime));
+        test.equal(docsReplaced, 0);
+        test.done();
+    });
+};
+
+exports['add doc3 for type test'] = function (test) {
+    var doc = new cl.Document();
+    var docId = '12';
+
+    doc.addField('name', 'asdssdf Jennings', cl.STORE_YES|cl.INDEX_TOKENIZED);
+    doc.addField('_type', 'contact', cl.STORE_YES|cl.INDEX_UNTOKENIZED);
+    doc.addField('timestamp', '1293765885000', cl.STORE_YES|cl.INDEX_UNTOKENIZED);
+
+    clucene.addDocument(docId, doc, indexPath, function(err, indexTime, docsReplaced) {
+        test.equal(err, null);
+        test.ok(is('Number', indexTime));
+        test.equal(docsReplaced, 0);
+        test.done();
+    });
+};
+
+exports['ensure 3 docs exist for type test'] = function (test) {     
+    clucene.search(indexPath, '_type:"contact"', function(err, results, searchTime) {
+        test.equal(err, null);
+        test.ok(is('Array', results));
+        test.ok(is('Number', searchTime));
+        test.equal(results.length, 3);
+        test.done();
+    });
+};
+ 
+exports['delete all docs of type'] = function (test) {        
+    clucene.deleteDocumentsByType('contact', indexPath, function(err, indexTime, docsDeleted) {
+        test.equal(err, null);
+        test.ok(is('Number', indexTime));
+        test.equal(docsDeleted, 3);
+        test.done();
+    });
+};
+    
+exports['ensure deleted docs of type are all gone'] = function (test) {            
+    clucene.search(indexPath, '_type:"contact"', function(err, results, searchTime) {
+        test.equal(err, null);
+        test.ok(is('Array', results));
+        test.ok(is('Number', searchTime));
+        test.equal(results.length, 0);
+        test.done();
+    });
 };
 
 function is(type, obj) {
